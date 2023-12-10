7d40746fbfdc714e7b0e33640c3cf96d391dafd0
waquikhan <103071047+waquikhan@users.noreply.github.com>
2023-12-10 12:27:01 +0530

    added the some styling to the link

--- src/Assessment-Components/Registration-Page.jsx
+++ src/Assessment-Components/Registration-Page.jsx
@@ -191,8 +191,8 @@ const RegistrationPage1 = () => {
 
       <div className='d-flex mt-2 justify-content-center'>
         <p className='me-1 ' style={{ color: 'grey' }}>Already have an account?</p>
-        <Link to="/userlogin">
-        <p style={{ color: "rgb(69, 239, 230)" }}>Sign In</p>
+        <Link to="/userlogin" style={{textDecoration:'none'}}>
+          <p style={{ color: "rgb(69, 239, 230)"}}>Sign In</p>
         </Link>
       </div>
     </Container>
